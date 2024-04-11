6b558bc961a082af0b599bbed26cbec6174e28a9
Tejaswi <201220014@nitdelhi.ac.in>
2024-04-11 18:58:49 +0530

    added all apis

---
+++ server.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const mongoose= require("mongoose");
+const express = require('express');
+const authRoute = require("./routes/auth")
+const jobRoute = require("./routes/job")
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log(" DB Connected!"))
+  .catch ((error)=>console.log("error",error));
+app.get("/api/health",(req,res)=>{
+    console.log("hey health");
+    res.json({
+        service:"Backend server",
+        time :new Date()
+    });
+});
+
+app.use("/api/v1/auth",authRoute);
+app.use("/api/v1/job", jobRoute);
+
+app.use("*",(req,res)=>{
+   res.status(404).json({errorMessage: "Route not found!"})
+});
+
+app.use((error,req,res,next)=>{
+    console.log(error);
+    res.status(500).json({errorMessage:"something went wrong!"})
+})
+
+
+const PORT = 3000;
+app.listen(PORT,()=>{
+    console.log(`Backend server running at ${PORT}`)
+});
